760496aef65f12c04e867fa38a3e5e295a281437
andershx <anderscookiez@gmail.com>
2025-08-13 09:47:40 +0200

    Add files via upload

--- app/casino/page.tsx
+++ app/casino/page.tsx
@@ -3,14 +3,18 @@ import { Dice } from "@/components/games/dice";
 import { Crash } from "@/components/games/crash";
 import { Mines } from "@/components/games/mines";
 import { Limbo } from "@/components/games/limbo";
-import { Plinko } from "@/components/games/plinko";
-export default function Casino(){ return (
-  <div className="space-y-8">
-    <h1 className="text-2xl font-bold">Casino</h1>
-    <Dice/>
-    <div id="crash"/>
-    <Crash/>
-    <Mines/>
-    <Limbo/>
-  </div>
-)}
+import Plinko from "@/components/games/plinko";
+
+export default function Casino(){
+  return (
+    <div className="space-y-8">
+      <h1 className="text-2xl font-bold">Casino</h1>
+      <Dice/>
+      <div id="crash"/>
+      <Crash/>
+      <Mines/>
+      <Limbo/>
+      <Plinko/>
+    </div>
+  );
+}
